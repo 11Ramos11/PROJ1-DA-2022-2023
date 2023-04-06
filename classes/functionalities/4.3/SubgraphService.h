e5161ad687509b4308055ef40981af6b2c6ffd65
Ramos <92692460+11Ramos11@users.noreply.github.com>
2023-04-06 16:51:20 +0100

    subgraphService time cplexity

--- classes/functionalities/4.3/SubgraphService.h
+++ classes/functionalities/4.3/SubgraphService.h
@@ -33,11 +33,15 @@ public:
 
     /** @brief Resets the subgraph to the original graph.
      *
+     * Time Complexity: O(1).
+     *
      * @return Void.
      */
     void resetSubgraph();
 
     /** @brief Deletes the specified station from the subgraph.
+     *
+     * Time Complexity: O(|V + E|), in which V is the number of Vertexes and E is the number of Edges, in the graph.
      *
      * @param id of int type.
      * @return Void.
@@ -45,6 +49,8 @@ public:
     void deleteStation(int id);
 
     /** @brief Deletes the specified edge from the subgraph.
+     *
+     * Time Complexity: O(E), in which E is the number of Edges, in the graph.
      *
      * @param orig of int type.
      * @param dest of int type.
@@ -54,6 +60,8 @@ public:
 
     /** @brief Deletes the all edges with a certain service from the subgraph.
      *
+     * Time Complexity: O(|V + E|), in which V is the number of Vertexes and E is the number of Edges, in the graph.
+     *
      * @param serviceType of ServiceType type.
      * @return Void.
      */
@@ -62,6 +70,8 @@ public:
     /** @brief Calculates the maximum number of trains that can simultaneously travel between
      * two specific stations in a network of reduced connectivity.
      *
+     * Time Complexity: O(|V * E^2|), in which V is the number of Vertexes and E is the number of Edges, in the graph.
+     *
      * @param source of int type.
      * @param target of int type.
      * @return The maximum number of trains.
@@ -69,6 +79,8 @@ public:
     int maxFlow(int source, int target);
 
     /** @brief Provides the top-k stations that are the most affected by the specified segment failure.
+     *
+     * Time Complexity: O(|V^3 * E^2|), in which V is the number of Vertexes and E is the number of Edges, in the graph.
      *
      * @param source of type int.
      * @param target of type int.
